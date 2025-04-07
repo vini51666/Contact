f379e24ee89e60f69921fd8f02c6b306edb83503
vini51666 <vinia.patel666@gmail.com>
2025-04-07 21:48:49 +0530

    Fix

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,15 +7,15 @@ const contact = () => {
 <h2>For any other Query you may also Contact us on:</h2>      
 <div class="contact-cards">
     <div class="card">
-        <img src="/images/email.img" alt="Email" />
+        <img src="/images/email.png" alt="Email" />
         <p><strong>Email:</strong> Falguni.Vahora@blissquants.com</p>
     </div>
     <div class="card">
-        <img src="/images/msg.img" alt="Messenger" />
+        <img src="/images/msg.png" alt="Messenger" />
         <p><strong>Messenger:</strong> Chat with us for quick response</p>
     </div>
     <div class="card">
-        <img src="/images/loc.img" alt="Address" />
+        <img src="/images/loc.png" alt="Address" />
         <p><strong>Address:</strong>D - 422,ITC Building, BlissQuants Datalytics LLP, Majuragate, Surat 395002</p>
     </div>
 </div>
